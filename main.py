daa79ef8b45652f7e8716f0cba24786e7d7d4353
theyadev <gtheya@gmail.com>
2021-10-18 14:05:37 +0200

    Added docs, added tests, formatting

--- main.py
+++ main.py
@@ -97,6 +97,7 @@ def linkParser(link: str):
     1 - https://osu.ppy.sh/beatmapsets/123456
     2 - https://osu.ppy.sh/beatmapsets/123456#osu/123456
     3 - https://osu.ppy.sh/b/123456
+    4 - https://osu.ppy.sh/beatmaps/123456
     """
 
     if not link.startswith(website_url):
@@ -126,6 +127,12 @@ def linkParser(link: str):
 
     elif link.startswith(f"{website_url}/b/"):
         link = link.replace(f"{website_url}/b/", "")
+        if link.isnumeric():
+            return None, int(link)
+        else:
+            return None, None
+    elif link.startswith(f"{website_url}/beatmaps/"):
+        link = link.replace(f"{website_url}/beatmaps/", "")
         if link.isnumeric():
             return None, int(link)
         else:
@@ -196,7 +203,7 @@ def saveBeatmapJSON(beatmap: Beatmap, genre: Genre):
     writeMapsMongo(beatmap_json)
 
     print(
-        f"{beatmap_json['artist']} - {beatmap_json['title']} has been added !")
+        f"{beatmap_json['artist']} - {beatmap_json['title']} [{beatmap_json['version']}] has been added !")
 
 
 def getBeatmapsFromBeatmapset(beatmapset_id: int = None, beatmap_id: int = None):
@@ -248,13 +255,61 @@ def addBeatmap(url: str, genre: str, import_beatmapset: bool):
         saveBeatmapJSON(beatmap, genre)
 
 
-if __name__ == "__main__":
-    addBeatmap("https://osu.ppy.sh/beatmapsets/1583851#osu/3235078",
-               "Classic", False)
+def truncateMaps():
+    db.maps.remove({})
+
+
+class TextColors:
+    BLACK = '\033[30m'
+    RED = '\033[31m'
+    GREEN = '\033[32m'
+    YELLOW = '\033[33m'
+    BLUE = '\033[34m'
+    MAGENTA = '\033[35m'
+    CYAN = '\033[36m'
+    WHITE = '\033[37m'
+    RESET = '\033[39m'
+
+
+def runTests():
+    truncateMaps()
+
+    # Test 1: Import beatmapset from supported link 1
+    print(f"{TextColors.YELLOW}Should import: Every Difficulty of AliA - Kakurenbo{TextColors.RESET}")
     addBeatmap("https://osu.ppy.sh/beatmapsets/955965",
                "Classic", True)
-    addBeatmap("https://osu.ppy.sh/b/1996988",
+
+    # Test 2: Import 1 Difficulty from supported link 2
+    print(
+        f"{TextColors.YELLOW}Should import: Shadow is the Light [blob]{TextColors.RESET}")
+    addBeatmap("https://osu.ppy.sh/beatmapsets/1583851#osu/3235078",
                "Classic", False)
+
+    # Test 3: Import beatmapset from supported link 2
+    print(
+        F"{TextColors.YELLOW}Should import: Kuyenda [Underwater]{TextColors.RESET}")
+    addBeatmap("https://osu.ppy.sh/beatmapsets/880539#osu/1929422", "Tech", True)
+
+    # Test 4: Import 1 Difficulty from supported link 3
+    print(
+        F"{TextColors.YELLOW}Should import: Feelsleft0ut [Nokris' Extreme]{TextColors.RESET}")
+    addBeatmap("https://osu.ppy.sh/b/1722775",
+               "Classic", False)
+
+    # Test 5: Import beatmapset from supported link 3
+    print(F"{TextColors.YELLOW}Should not import: AliA - Kakurenbo{TextColors.RESET}")
     addBeatmap("https://osu.ppy.sh/b/1996988",
                "Classic", True)
-    addBeatmap("https://osu.ppy.sh/beatmapsets/880539#osu/1929422", "Tech", True)
+
+    # Test 6: Import 1 Difficulty from supported link 4
+    print(
+        F"{TextColors.YELLOW}Should import: Kirameki Inokori Daisensou [F4UZ4N's Expert!]{TextColors.RESET}")
+    addBeatmap("https://osu.ppy.sh/beatmaps/2319298", "Classic", False)
+
+    # Test 7: Import beatmapset from supported link 4
+    print(F"{TextColors.YELLOW}Should import every difficulty from *Feels Seasickness...*{TextColors.RESET}")
+    addBeatmap("https://osu.ppy.sh/beatmaps/1929269", "Tech", True)
+
+
+if __name__ == "__main__":
+    runTests()
